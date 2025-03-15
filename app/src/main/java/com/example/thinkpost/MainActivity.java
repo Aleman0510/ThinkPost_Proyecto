00f112dd0ce22b5a2e1adb8b52877ec85e949100
Aram Aleman <alemancardenasaram@gmail.com>
2025-03-15 15:54:09 -0600

    conexion a la bd

--- app/src/main/java/com/example/thinkpost/MainActivity.java
+++ app/src/main/java/com/example/thinkpost/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.thinkpost;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -19,7 +29,31 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+// Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("username", "comepija999");
+        user.put("prfpicture", "Lovelace");
+        user.put("password", 1815);
+        user.put("email", "comepijas@gmail.com");
+        user.put("descripcion", "Luis es gay");
 
+
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
