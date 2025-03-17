e21237315f306408a138b9e9cdd2d368bbb7ca36
ram <hector.ara98@outlook.com>
2025-03-17 13:37:45 -0600

    Se agregó funcionalidad para el boton de postear en HomeActivity.xml. Archivos agregados:
    - CreatePostActivity.java
    - slide_in_left.xml
    - slide_in_right.xml
    
    Se modificaron los archivos:
    - HomeActivity.java
    - AndroidManifest.xml

---
+++ app/src/main/java/com/example/thinkpost/CreatePostActivity.java
@@ -0,0 +1,12 @@
+package com.example.thinkpost;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreatePostActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post_publicacion); // Asegúrate que el nombre del layout coincida
+    }
+}
